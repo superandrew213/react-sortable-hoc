95a684340bca8d635e1cabf65e83580e76217ed6
Andrew Shini <superandrew213@gmail.com>
2019-01-16 10:58:01 +1000

    Format

--- src/SortableContainer/index.js
+++ src/SortableContainer/index.js
@@ -664,10 +664,8 @@ export default function sortableContainer(
               translate.x = this.width + this.marginOffset.x;
               if (
                 nextNode &&
-                (
-                  edgeOffset.left + translate.x >
+                edgeOffset.left + translate.x >
                   this.containerBoundingRect.width - offset.width
-                )
               ) {
                 // If it moves passed the right bounds, then animate it to the first position of the next row.
                 // We just use the offset of the next node to calculate where to move, because that node's original position
@@ -692,10 +690,8 @@ export default function sortableContainer(
               translate.x = -(this.width + this.marginOffset.x);
               if (
                 prevNode &&
-                (
-                  edgeOffset.left + translate.x <
+                edgeOffset.left + translate.x <
                   this.containerBoundingRect.left + offset.width
-                )
               ) {
                 // If it moves passed the left bounds, then animate it to the last position of the previous row.
                 // We just use the offset of the previous node to calculate where to move, because that node's original position
